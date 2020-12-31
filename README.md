f9f5f2073ca3619ca473d5ac23918ff77ca34ebf
BuildTools <unconfigured@null.spigotmc.org>
2020-12-31 16:20:46 +0800

    v1.0

---
+++ README.md
@@ -0,0 +1,21 @@
+# cf-ddns
+
+## usage
+
+```
+> cf-ddns -h
+usage: cf-ddns [-6] -t <API_TOKEN> -d <DOMAIN>
+
+optional arguments:
+  -h, --help      show this help message and exit
+  -6              enable IPv6 mode
+  -t <API_TOKEN>  your cf api token
+  -d <DOMAIN>     your cf domain name
+
+
+```
+
+## auto update
+
+- win10 : 工作排程器
+- linux : crontab
---
+++ cf_ddns.py
@@ -0,0 +1,86 @@
+import requests
+import argparse
+
+endpoints = 'https://api.cloudflare.com/client/v4/'
+ip_url = {
+	'v4_ori': 'http://ip1.dynupdate.no-ip.com/',
+	'v4_bak': 'http://ip1.dynupdate.no-ip.com:8245/',
+	'v6_ori': 'http://ip1.dynupdate6.no-ip.com/',
+	'v6_bak': 'http://ip1.dynupdate6.no-ip.com:8245/'
+}
+
+def getZoneID(dn):
+	tmp = dn.split('.')
+	top_lv = tmp[-2] + '.' + tmp[-1]
+	params = {
+		"name": top_lv
+	}
+	r = requests.get(endpoints + 'zones', headers=headers, params=params)
+	return r.json()['result'][0]['id']
+
+def getDomainID(dn, zID, type='A'):
+	params = {
+		"name": dn,
+		"type": type
+	}
+	r = requests.get(endpoints + 'zones/' + zID + '/dns_records', headers=headers, params=params)
+	return r.json()['result'][0]['id']
+
+def get_ip_addr(v):
+	try:
+		r = requests.get(ip_url[v + '_ori'])
+	except:
+		r = requests.get(ip_url[v + '_bak'])
+	return r.text
+
+def update_dns(domain, ip_addr_4, ip_addr_6):
+	zID = getZoneID(domain)
+	domain_id_A = getDomainID(domain, zID)
+
+	print('#--------------------#')
+	print(' ', domain)
+
+	data = {
+		"type": "A",
+		"name": domain,
+		"content": ip_addr_4
+	}
+	r = requests.put(endpoints + 'zones/' + zID + '/dns_records/' + domain_id_A, headers=headers, json=data)
+	print('  ipv4 :', r.json()['success'])
+
+	if ip_addr_6 != '':
+		domain_id_AAAA = getDomainID(domain, zID, 'AAAA')
+		data = {
+			"type": "AAAA",
+			"name": domain,
+			"content": ip_addr_6
+		}
+		r = requests.put(endpoints + 'zones/' + zID + '/dns_records/' + domain_id_AAAA, headers=headers, json=data)
+		print('  ipv6 :', r.json()['success'])
+
+	print('#--------------------#')
+
+if __name__ == '__main__':
+	# args parser
+	parser = argparse.ArgumentParser(
+		prog='cf-ddns',
+		usage='cf-ddns [-6] -t <API_TOKEN> -d <DOMAIN>',
+	)
+	parser.add_argument('-6', action='store_true', help='enable IPv6 mode')
+	parser.add_argument('-t', metavar='<API_TOKEN>', required=True, help='your cf api token')
+	parser.add_argument('-d', metavar='<DOMAIN>', required=True, help='your cf domain name')
+	args = vars(parser.parse_args())
+
+	kwargs = {
+		'domain': args['d'],
+		'ip_addr_4': get_ip_addr('v4'),
+		'ip_addr_6': get_ip_addr('v6') if args['6'] else '',
+	}
+
+	api_token = args['t']
+	headers = {
+		"Authorization": f"Bearer {api_token}",
+		"Content-Type": "application/json"
+	}
+
+	update_dns(**kwargs)
